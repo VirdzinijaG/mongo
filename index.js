868fcde2da66a58c1f23212459e2a79085b2e61d
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-06 12:37:44 +0300

    update

--- index.js
+++ index.js
@@ -26,7 +26,19 @@ try {
 
     // const zmones = await db.collection("zmones").find().toArray(); // atspausdinami visi duomenys is duomenu bazes masyve
     // const zmones = await db.collection("zmones").find({vardas: "Jonas"}).toArray(); // atspaudins reiksmes, kur vardas yra Jonas
-    const zmones = await db.collection("zmones").find({ alga: { $gt: 200 } }).toArray(); // atspausdins reiksmes, kur alga didesne nei 200
+    // const zmones = await db.collection("zmones").find({alga: {$gt: 200}}).toArray(); // atspausdins reiksmes, kur alga didesne nei 200
+
+    await db.collection("zmones").updateOne( // keiciama informacija apie zmogu
+        { _id: ObjectId("6135e0e54e781c4912caa704") }, // nurodomas koks id, value
+        {
+            $set: { // set nustatyti naujas reiksmes laukams
+                pavarde: "pakeista pavarde", // pakeista reiksme
+                gimimoData: new Date("2021-02-03") // pridetas naujas laukas
+            },
+            $push: { kontaktai: { tipas: "darbo", reiksme: "86985653", komentaras: "nuo 8 iki 17 val" } } // push pridedamas naujas objektas
+        }
+    )
+    const zmones = await db.collection("zmones").find().toArray();
     console.log(zmones);
 }
 catch (err) {
