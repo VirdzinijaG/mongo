b59ee8cd41d84a48cebff505d521b4eda5b3b3f6
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-09-06 11:50:04 +0300

    find

--- index.js
+++ index.js
@@ -8,23 +8,25 @@ try {
 
     const db = client.db("adresai");
 
-    await db.collection("zmones").insertOne({ // pridedamas naujas zmogus
-        vardas: "Vardenis",
-        pavarde: "pavardenis",
-        alga: 659.36,
-        kontaktai: [
-            {
-                tipas: "mob",
-                reiksme: "865963269"
-            },
-            {
-                tipas: "email",
-                reiksme: "vardenis@gmail.com"
-            }
-        ]
-    });
+    // await db.collection("zmones").insertOne({ // pridedamas naujas zmogus
+    //     vardas: "Vardenis",
+    //     pavarde: "pavardenis",
+    //     alga: 659.36,
+    //     kontaktai: [
+    //         {
+    //             tipas: "mob",
+    //             reiksme: "865963269"
+    //         },
+    //         {
+    //             tipas: "email",
+    //             reiksme: "vardenis@gmail.com"
+    //         }
+    //     ]
+    // });
 
-    const zmones = await db.collection("zmones").find().toArray(); // atspausdinami visi duomenys is duomenu bazes masyve
+    // const zmones = await db.collection("zmones").find().toArray(); // atspausdinami visi duomenys is duomenu bazes masyve
+    // const zmones = await db.collection("zmones").find({vardas: "Jonas"}).toArray(); // atspaudins reiksmes, kur vardas yra Jonas
+    const zmones = await db.collection("zmones").find({ alga: { $gt: 200 } }).toArray(); // atspausdins reiksmes, kur alga didesne nei 200
     console.log(zmones);
 }
 catch (err) {
